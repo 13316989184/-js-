c2a69b0a6c88ff5c85ced9fb47fb24c5420c767f
liubo <13316989184@163.com>
2019-05-19 13:45:35 +0800

    深入理解monad

--- functional-playground/play.js
+++ functional-playground/play.js
@@ -1,216 +1,143 @@
-import { Container, MayBe, arrayUtils, Some, Nothing } from '../lib/es6-functional.js'
-const fetch = require('node-fetch');
-let https = require('https');
+import {Container,MayBe,arrayUtils,Some,Nothing} from '../lib/es6-functional.js'
 
-function* gen() {
-    return 'first generator';
+var request = require('sync-request');
+
+
+let searchReddit = (search) => {
+    let response  
+    try{
+       response = JSON.parse(request('GET',"https://www.reddit.com/search.json?q=" + encodeURI(search) + "&limit=2").getBody('utf8'))
+    }catch(err) {
+        response = { message: "Something went wrong" , errorCode: err['statusCode'] }
+    }
+    return response
 }
 
-let generatorResult = gen()
+let getComments = (link) => {
+    let response
+    try {
+        console.log("https://www.reddit.com/" + link)
+        response = JSON.parse(request('GET',"https://www.reddit.com/" + link).getBody('utf8'))
+    } catch(err) {
+        console.log(err)
+        response = { message: "Something went wrong" , errorCode: err['statusCode'] }
+    }
 
-console.log("For first time",generatorResult.next().value)
-console.log("For second time time",generatorResult.next().value)
-
-let generatorResult2 = gen()
-generatorResult = gen() //re-create again
-
-console.log("First generator",generatorResult.next().value)
-console.log("Second generator",generatorResult2.next().value)
-
-function* generatorSequence() {
-    yield 'first';
-    yield 'second';
-    yield 'third';
+    return response 
 }
 
-let generatorSequenceResult = generatorSequence();
-
-console.log("\n\n\n")
-console.log('First time sequence value',generatorSequenceResult.next().value)
-console.log('Second time sequence value',generatorSequenceResult.next().value)
-console.log('thrid time sequence value',generatorSequenceResult.next().value)
-
-
-generatorSequenceResult = generatorSequence();
-
-console.log("\n\n")
-console.log('done value for the first time',generatorSequenceResult.next())
-console.log('done value for the second time',generatorSequenceResult.next())
-console.log('done value for the third time',generatorSequenceResult.next())
-console.log('done value for the fourth time',generatorSequenceResult.next())
-
-console.log("\n\n")
-for(let value of generatorSequence())
-    console.log("for of value of generatorSequence is",value)
-
-
-//passing data to generators
-function* sayFullName() {
-    var firstName = yield 
-    var secondName = yield
-    console.log(firstName + secondName);
-}
-
-let fullName = sayFullName()
-fullName.next()
-fullName.next('anto')
-fullName.next('aravinth')
-
-let getDataOne = (cb) => {
-    setTimeout(function(){
-        //calling the callback
-        cb('dummy data one')
-    }, 1000);
-}
-
-let getDataTwo = (cb) => {
-    setTimeout(function(){
-        //calling the callback
-        cb('dummy data two')
-    }, 1000);
-}
-
-getDataOne((data) => console.log("data received",data))
-getDataTwo((data) => console.log("data received",data))
-
-//using generator
-let generator;
-getDataOne = () => {
-    setTimeout(function(){
-        //call the generator and
-        //pass data via `next`
-        generator.next('dummy data one')
-    }, 1000);
-}
-
-getDataTwo = () => {
-    setTimeout(function(){
-        //call the generator and
-        //pass data via `next`
-        generator.next('dummy data two')
-    }, 1000);
-}
-
-function* main() {
-    let dataOne = yield getDataOne();
-    let dataTwo = yield getDataTwo();
-    console.log("data one",dataOne)
-    console.log("data two",dataTwo)
-}
-
-generator = main()
-generator.next()
-
-
-function httpGetAsync(url,callback) {
-    return https.get(url, 
-        function(response) {
-            var body = '';
-            response.on('data', function(d) {
-                body += d;
-            });
-            response.on('end', function() {
-                let parsed = JSON.parse(body)
-                callback(parsed)
-            })
-        }
-    );
-
-}
-
-httpGetAsync('https://www.reddit.com/r/pics/.json',(picJson)=> {
-    httpGetAsync(picJson.data.children[0].data.url+".json",(firstPicRedditData) => {
-        console.log(firstPicRedditData)
+let mergeTitleAndComments = (searchMayBe,commentsMayBe) => {
+    searchMayBe.map((value) => {
+        commentsMayBe.map((commentsValue) => {
+            console.log(value)
+            console.log(commentsValue)
+        })
     })
+}
+
+let mergeViaMayBe = (searchText) => {
+    let redditMayBe = MayBe.of(searchReddit(searchText))
+    let ans = redditMayBe
+               .map((arr) => arr['data'])
+               .map((arr) => arr['children'])
+               .map((arr) => arrayUtils.map(arr,(x) => {
+                        return {
+                            title : x['data'].title,
+                            permalink : x['data'].permalink
+                        }
+                    } 
+                ))
+               .map((obj) => arrayUtils.map(obj, (x) => {
+                    return {
+                        title: x.title,
+                       comments: MayBe.of(getComments(x.permalink.replace("?ref=search_posts",".json")))
+                    }
+               }))
+
+   return ans;
+}
+
+
+let mergeViaJoin = (searchText) => {
+    let redditMayBe = MayBe.of(searchReddit(searchText))
+    let ans = redditMayBe.map((arr) => arr['data'])
+               .map((arr) => arr['children'])
+               .map((arr) => arrayUtils.map(arr,(x) => {
+                        return {
+                            title : x['data'].title,
+                            permalink : x['data'].permalink
+                        }
+                    } 
+                ))
+               .map((obj) => arrayUtils.map(obj, (x) => {
+                    return {
+                        title: x.title,
+                       comments: MayBe.of(getComments(x.permalink.replace("?ref=search_posts",".json"))).join()
+                    }
+               }))
+               .join()
+
+   return ans;
+}
+
+let answer;
+answer = mergeViaMayBe("functional programming")
+
+console.log(answer)
+
+// Via MayBe functor 
+/*
+    See how deep we map to get the answers
+*/
+answer.map((result) => {
+    arrayUtils.map(result,(mergeResults) => {
+        mergeResults.comments.map(comment => {
+            console.log(comment)
+        })
+    }) 
+})
+
+//simple examples of join
+let joinExample = MayBe.of(MayBe.of(5))
+console.log("Without Join Example",joinExample.map((outsideMayBe) => {
+    return outsideMayBe.map((x) => x + 4)
+}))
+console.log("Join Example",joinExample.join().map((v) => v + 4))
+
+//trying our old problem with join
+answer = mergeViaJoin("functional programming")
+
+console.log(answer)
+
+arrayUtils.map(answer,a => {
+    console.log(a.comments)
 })
 
 
-function request(url) {
-    httpGetAsync( url, function(response){
-        generator.next( response );
-    } );
+let mergeViaChain = (searchText) => {
+    let redditMayBe = MayBe.of(searchReddit(searchText))
+    let ans = redditMayBe.map((arr) => arr['data'])
+               .map((arr) => arr['children'])
+               .map((arr) => arrayUtils.map(arr,(x) => {
+                        return {
+                            title : x['data'].title,
+                            permalink : x['data'].permalink
+                        }
+                    } 
+                ))
+               .chain((obj) => arrayUtils.map(obj, (x) => {
+                    return {
+                       title: x.title,
+                       comments: MayBe.of(getComments(x.permalink.replace("?ref=search_posts",".json"))).chain(x => {
+                            return x.length
+                       })
+                    }
+               }))
+
+   return ans;
 }
 
-function *main() {
-    let picturesJson = yield request( "https://www.reddit.com/r/pics/.json" );
-    let firstPictureData = yield request(picturesJson.data.children[0].data.url+".json")
-}
+//trying our old problem with chain
+answer = mergeViaChain("functional programming")
 
-generator = main()
-generator.next()
-
-/**************************************************************************************************************************************************** */
-//Promise Examples
-let grade = "A+";
-let examResults = new Promise(
-    function (resolve, reject) {
-        if (grade == "A+")
-            resolve("You will get an XBOX");
-        else
-            reject("Better luck next time");
-    }
-);
-
-let conductExams = () => {
-    examResults
-        .then(x => console.log(x)) // captures resolve and logs "You will get an XBOX"
-        .catch(x => console.error(x)); // captures rejection and logs "Better luck next time"
-};
-
-conductExams();
-
-// /**************************************************************************************************************************************************** */
-// Simple Async / Await example using Promise
-function fetchTextByPromise() {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve("es8");
-        }, 2000);
-    });
-}
-
-let sayHello = async () => {
-    const externalFetchedText = await fetchTextByPromise();
-    console.log(`Response from SayHello: Hello, ${externalFetchedText}`); // Hello, es8
-}
-
-console.log("Before Calling SayHello");
-sayHello(); // this returns after 2 seconds
-console.log("After Calling SayHello"); // this will be printed before SayHello, because SayHello is a non blocking call
-
-/**************************************************************************************************************************************************** */
-// Simple Async / Await example using Remote API Call
-
-// returns a promise
-const getAsync = (url) => {
-    return fetch(url)
-        .then(x => x)
-        .catch(x =>
-            console.log("Error in getAsync:" + x)
-        );
-}
-
-// 'async' con only be used in functions where 'await' is used
-async function getAsyncCaller() {
-    try {
-        // https://jsonplaceholder.typicode.com/users is a sample API which returns a JSON Array of dummy users
-        const response = await getAsync("https://jsonplaceholder.typicode.com/users"); // pause until promise completes
-        const result = await response.json(); //removing .json here demonstrates the error handling in promises
-        console.log("GetAsync fetched " + result.length + " results");
-        return result;
-    } catch (error) {
-        await Promise.reject("Error in getAsyncCaller:" + error.message);
-    }
-}
-
-getAsyncCaller()
-    .then(async (x) => {
-        console.log("Call to GetAsync function completed");
-        const website = await getAsync("http://" + x[0].website);
-        console.log("The website (http://" + x[0].website + ") content length is " + website.toString().length + " bytes");
-    })
-    .catch(x => console.log("Error: " + x)); // Promise.Reject is caught here, the error message can be used to perform custom error handling
-
-console.log("This message is displayed while waiting for async operation to complete, you can do any compute here...");
-
-/**************************************************************************************************************************************************** */
+console.log(answer)
